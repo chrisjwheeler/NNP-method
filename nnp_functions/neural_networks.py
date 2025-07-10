0156cf98c4d2021b79867b5965c32eaaaa1cb079
chrisjwheeler <chrisjamesw23@gmail.com>
2025-07-10 16:59:53 +0100

    Add ESS experiment.
    Added ess experiment which demonstrates that the ess of the nn vs
    boostrap underperforms. However want to see how the tau effects this as
    we saw this previously. Currently we have a very small tau.

---
+++ nnp_functions/neural_networks.py
@@ -0,0 +1,25 @@
+import jax
+import equinox as eqx
+
+class FeedForwardNetwork(eqx.Module):
+    """Feedforward neural network predicting mean and std of a normal distribution.
+
+    Input: 2D (X_{t-1}, R_t)
+    Output: mean, std (std > 0 via softplus)
+    """
+    layers: list[eqx.nn.Linear]
+
+    def __init__(self, n_layers: int, hidden_dim: int, key: jax.random.PRNGKey):
+        subkeys = jax.random.split(key, n_layers + 1)
+        layers = [eqx.nn.Linear(2, hidden_dim, key=subkeys[0])]
+        layers += [eqx.nn.Linear(hidden_dim, hidden_dim, key=subkeys[i]) for i in range(1, n_layers)]
+        layers.append(eqx.nn.Linear(hidden_dim, 2, key=subkeys[-1]))
+        self.layers = layers
+
+    def __call__(self, x: jax.Array) -> tuple[jax.Array, jax.Array]:
+        for layer in self.layers[:-1]:
+            x = jax.nn.relu(layer(x))
+        x = self.layers[-1](x)
+        mean, log_std = x[0], x[1]
+        std = jax.nn.softplus(log_std) + 1e-24
+        return [mean, std]
