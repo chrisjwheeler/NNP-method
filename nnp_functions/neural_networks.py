2b5bd8f310926cc44a7893b1f71c240bf2874189
chrisjwheeler <chrisjamesw23@gmail.com>
2025-07-13 19:21:20 +0100

    Implement mixture distribution.

--- nnp_functions/neural_networks.py
+++ nnp_functions/neural_networks.py
@@ -47,3 +47,32 @@ class t_FeedForwardNetwork(eqx.Module):
         std = jax.nn.softplus(log_std) + 1e-24
         def_freedom = jax.nn.softplus(def_freedom) + 1e-24
         return mean, std, def_freedom
+
+
+class mix_FeedForwardNetwork(eqx.Module):
+    """Feedforward neural network predicting mean and std of a normal distribution.
+
+    Input: 2D (X_{t-1}, R_t)
+    Output: mean, std (std > 0 via softplus)
+    """
+    layers: list[eqx.nn.Linear]
+    n_mix: int
+
+    def __init__(self, n_layers: int, hidden_dim: int, n_mix: int, key: jax.random.PRNGKey):
+        self.n_mix = n_mix
+        subkeys = jax.random.split(key, n_layers + 1)
+        layers = [eqx.nn.Linear(2, hidden_dim, key=subkeys[0])]
+        layers += [eqx.nn.Linear(hidden_dim, hidden_dim, key=subkeys[i]) for i in range(1, n_layers)]
+        layers.append(eqx.nn.Linear(hidden_dim, 3 * n_mix, key=subkeys[-1]))
+        self.layers = layers
+
+    def __call__(self, x: jax.Array) -> tuple[jax.Array, jax.Array]:
+        for layer in self.layers[:-1]:
+            x = jax.nn.relu(layer(x))
+        x = self.layers[-1](x)
+        mean, log_std, weights = x[0:self.n_mix], x[self.n_mix:2*self.n_mix], x[2*self.n_mix:]
+        
+        std_val = jax.nn.softplus(log_std) + 1e-24
+        weights = jax.nn.softmax(weights)
+        
+        return mean, std_val, weights
